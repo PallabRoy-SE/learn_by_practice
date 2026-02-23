ef90722e359d1965e209557e49fa3d41728121d4
PALLAB ROY <76547199+PallabRoy-SE@users.noreply.github.com>
2026-02-23 22:11:32 +0530

    23Feb2026

--- 21Feb2026/main.py
+++ 21Feb2026/main.py
@@ -1,6 +1,25 @@
 from fastapi import FastAPI
 
+MOCK_USERS = {
+    1: {"name": "Alice", "role": "admin"},
+    2: {"name": "Bob", "role": "user"},
+    3: {"name": "Charlie", "role": "user"},
+}
+
 app = FastAPI()
+
+
+@app.get("/users/{user_id}")
+def get_user(user_id: int):
+    return MOCK_USERS.get(user_id)
+
+
+@app.get("/users")
+def get_users(role: str | None = None):
+    if role is not None:
+        return [user for id, user in MOCK_USERS.items() if user.get("role") == role]
+    else:
+        return MOCK_USERS
 
 
 @app.get("/")
